a3bde13a28843ac60ff5ac87993d080cd151ec27
Mukhtar Axmed <143383104+MoDev40@users.noreply.github.com>
2024-03-18 03:10:19 +0000

    adding relationship

--- app/drizzle/schema.ts
+++ app/drizzle/schema.ts
@@ -1,16 +1,21 @@
-import { relations } from "drizzle-orm";
-import { serial, varchar } from "drizzle-orm/pg-core";
+import { boolean, decimal, integer, serial, timestamp, varchar } from "drizzle-orm/pg-core";
 import { pgTable } from "drizzle-orm/pg-core";
 
 export const user = pgTable("user",{
     id:serial("id").primaryKey(),
     name:varchar("name"),
-    email:varchar("email").unique().notNull()
+    email:varchar("email").unique().notNull(),
+    createdAt:timestamp("createdAt").defaultNow()
 })
 
 export const book = pgTable("book",{
     id:serial("id").primaryKey(),
-    name:varchar("title"),
+    name:varchar("title").notNull(),
     author:varchar("author"),
+    isFree:boolean("isFree").default(false),
+    pdfUrl:varchar("pdf").notNull(),
     imageUrl:varchar("image"),
+    createdAt:timestamp("createdAt").defaultNow(),
+    price:decimal("price"),
+    user_id:integer("user_id").notNull().references(()=> user.id)
 })
